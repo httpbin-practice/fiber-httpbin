1ca1b9a9fcfbaa0e4b76d4ebd0a5861d17c43bee
TaceyWong <xinyong.wang@xtalpi.com>
2021-03-17 18:49:01 +0800

    x

--- request_inspect.go
+++ request_inspect.go
@@ -1,15 +1,18 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func GetHeaders(c *fiber.Ctx) error {
 	return c.SendString("get headers")
 }
 
 func GetIP(c *fiber.Ctx) error {
-	return c.SendString("get ip")
+	return c.JSON(map[string]string{"origin": c.IP()})
 }
 
 func GetUserAgent(c *fiber.Ctx) error {
-	return c.SendString("get user agent")
+	userAgent := string(c.Context().UserAgent())
+	return c.JSON(map[string]string{"User-Agent": userAgent})
 }
