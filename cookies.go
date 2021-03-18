752c0bda1ff87f0f695622aadf54ab982d26aebf
TaceyWong <xinyong.wang@xtalpi.com>
2021-03-18 11:52:01 +0800

    cookies

--- cookies.go
+++ cookies.go
@@ -1,9 +1,15 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func GetCookies(c *fiber.Ctx) error {
-	return c.SendString("get cookies")
+	cookies := map[string]string{}
+	c.Request().Header.VisitAllCookie(func(k, v []byte) {
+		cookies[string(k)] = string(v)
+	})
+	return c.JSON(map[string]interface{}{"cookies": cookies})
 }
 
 func GetCookiesDelete(c *fiber.Ctx) error {
@@ -11,9 +17,32 @@ func GetCookiesDelete(c *fiber.Ctx) error {
 }
 
 func GetCookiesSet(c *fiber.Ctx) error {
-	return c.SendString("get cookies set")
+	cookies := map[string]string{}
+	c.Request().Header.VisitAllCookie(func(k, v []byte) {
+		cookies[string(k)] = string(v)
+	})
+	c.Context().QueryArgs().VisitAll(func(k, v []byte) {
+		name, value := string(k), string(v)
+		c.Cookie(&fiber.Cookie{
+			Name:  name,
+			Value: value,
+		})
+		cookies[name] = value
+	})
+
+	return c.JSON(map[string]interface{}{"cookies": cookies})
 }
 
 func GetCookiesSetValue(c *fiber.Ctx) error {
-	return c.SendString("get cookies set value")
+	name, value := c.Params("name"), c.Params("value")
+	cookies := map[string]string{}
+	c.Cookie(&fiber.Cookie{
+		Name:  name,
+		Value: value,
+	})
+	c.Request().Header.VisitAllCookie(func(k, v []byte) {
+		cookies[string(k)] = string(v)
+	})
+	cookies[name] = value
+	return c.JSON(map[string]interface{}{"cookies": cookies})
 }
